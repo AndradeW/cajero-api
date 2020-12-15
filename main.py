74e1ce359b0d3dd2cd9a3c596189ba0db959a828
AndradeW <wilson00749@gmail.com>
2020-12-14 21:53:06 -0500

    modificado ruta del front en main.py 2

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ api = FastAPI()
 from fastapi.middleware.cors import CORSMiddleware
 origins = [
 "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
-"http://localhost", "http://localhost:8080", "http://127.0.0.1:8080", "https://aw-cajero-app.herokuapp.com",
+"http://localhost", "http://localhost:8080", "http://127.0.0.1:8080", "https://aw-cajero-app.herokuapp.com", "https://aw-cajero-back.herokuapp.com/user/balance/camilo24",
 ]
 api.add_middleware(
 CORSMiddleware, allow_origins=origins,
